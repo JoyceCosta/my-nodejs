378abada288a20e49b429fa385f78531055de3ce
Joyce Costa <joycecosta90@gmail.com>
2020-11-18 23:09:34 -0300

    route

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {}); 
+const server = http.createServer((req, res) => {
+  const url = req.url;
+   if (url === '/') {
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<head><title>Tarefa 1</title></head>');
+      res.write("<body><p>Bienvenue!</p></body>");
+      res.write("</html>");
+      return res.end();
+   }
+   if (url === '/users') {
+
+   }
+}); 
 
 server.listen(3000);
+
