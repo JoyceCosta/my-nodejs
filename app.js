12ea3a1d8a070c2108da353a3222a1195f08d7c6
Joyce Costa <joycecosta90@gmail.com>
2020-11-18 23:20:32 -0300

    lista de usuários

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ const server = http.createServer((req, res) => {
       return res.end();
    }
    if (url === '/users') {
-
+      res.setHeader("Content-Type", "text/html");
+			res.write("<html>");
+			res.write("<head><title>Usuários</title></head>");
+      res.write("<body><ul><li>Luffy</li><li>Zoro</li><li>Nami</li><li>Usopp</li><li>Sanji</li><li>Chopper</li><li>Robin</li><li>Franky</li><li>Brook</li></ul></body>");
+			res.write("</html>");
+			res.end();
    }
 }); 
 
